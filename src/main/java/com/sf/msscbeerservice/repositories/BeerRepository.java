2b63b0d8ab4c201460164ec61d57a4a59df42d41
Muhammad Arshad <asim.mughal61@gmail.com>
2021-02-07 18:16:51 -0600

    added beer repository. Closes #6

---
+++ src/main/java/com/sf/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.sf.msscbeerservice.repositories;
+
+import com.sf.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
