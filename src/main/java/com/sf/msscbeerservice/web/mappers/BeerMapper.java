a263d31afd2d1954ea681cdd9c5abdefee24ca5e
Muhammad Arshad <asim.mughal61@gmail.com>
2021-02-11 01:24:48 -0600

    added Mapstruct for type conversion. Closes #10

---
+++ src/main/java/com/sf/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,11 @@
+package com.sf.msscbeerservice.web.mappers;
+
+import com.sf.msscbeerservice.domain.Beer;
+import com.sf.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+    BeerDto BeerToBeerDto(Beer beer);
+    Beer BeerDtoToBeer(BeerDto beerDto);
+}
