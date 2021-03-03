b21f6f7355680e1320489723830ac6776cb90321
Muhammad Arshad <asim.mughal61@gmail.com>
2021-03-02 23:55:19 -0600

    Add HikariCP Config and debug and MySql-Dialect correction, closes #25

--- src/main/java/com/sf/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/sf/msscbeerservice/bootstrap/BeerLoader.java
@@ -3,22 +3,19 @@ package com.sf.msscbeerservice.bootstrap;
 
 import com.sf.msscbeerservice.domain.Beer;
 import com.sf.msscbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-
+@RequiredArgsConstructor
 @Component
 public class BeerLoader implements CommandLineRunner {
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
     private final BeerRepository beerRepository;
-
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
 
     @Override
     public void run(String... args) {
